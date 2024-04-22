2452741d0492fa8800b56507b8cfb6dfb4b433e5
Sandeepshaikust <167755610+Sandeepshaikust@users.noreply.github.com>
2024-04-22 14:41:04 +0530

    Add files via upload

---
+++ testshapes.java
@@ -0,0 +1,13 @@
+package Javahandsone;
+
+public class testshapes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+circle c=new circle();
+c.calculatearea();
+rectangle r=new rectangle();
+r.caluculatearea();
+	}
+
+}
