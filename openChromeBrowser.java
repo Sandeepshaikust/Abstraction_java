7b3a2afb3a40401995253dc39424475454a68c38
Sandeepshaikust <167755610+Sandeepshaikust@users.noreply.github.com>
2024-04-29 15:33:59 +0530

    Add files via upload

---
+++ openChromeBrowser.java
@@ -0,0 +1,23 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class openChromeBrowser {
+	
+WebDriver driver;
+	
+	public WebDriver openChrome() {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver-win64\\chromedriver.exe");
+	
+	ChromeOptions options = new ChromeOptions();
+    options.setBrowserVersion("124");
+    driver = new ChromeDriver(options);
+    return driver;
+    
+	}
+
+}
+
+
